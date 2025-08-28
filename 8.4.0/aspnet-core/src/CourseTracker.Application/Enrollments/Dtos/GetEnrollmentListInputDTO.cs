0cce75ef5f2cfde5bff4c5c2cee1cf4cca3eefc7
jawad altaf <jawadaltaf004@gmail.com>
2025-08-28 19:55:44 +0500

    Final testing and bug fixes

---
+++ 8.4.0/aspnet-core/src/CourseTracker.Application/Enrollments/Dtos/GetEnrollmentListInputDTO.cs
@@ -0,0 +1,16 @@
+﻿using Abp.Application.Services.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseTracker.Enrollments.Dtos
+{
+    public class GetEnrollmentListInputDTO: PagedAndSortedResultRequestDto
+    {
+        public string Keyword { get; set; }
+        public int PageNumber { get; set; }
+        public int PageSize { get; set; }
+    }
+}
---
+++ 8.4.0/aspnet-core/src/CourseTracker.Application/Learners_/Dtos/GetLearnerListInputDTO.cs
@@ -0,0 +1,16 @@
+﻿using Abp.Application.Services.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseTracker.Learners_.Dtos
+{
+    public class GetLearnerListInputDTO: PagedAndSortedResultRequestDto
+    {
+        public string Keyword { get; set; }
+        public int PageNumber { get; set; }
+        public int PageSize { get; set; }
+    }
+}
